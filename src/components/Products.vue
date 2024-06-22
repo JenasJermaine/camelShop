df7d599fa1239929dbfe3625434e26b95ee4a88d
Jenas Jermaine <barakajenas@gmail.com>
2024-06-22 09:53:14 +0300

    cart

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,43 +1,96 @@
 <template>
     <h1>Confectionery</h1>
-    <div>
-    <v-row>
-        <v-col cols="12" md="3"  v-for="product in stockDetails" :key=product>
+    <p>{{total}}</p>
     <v-card
-    class="mx-auto my-12"
-    max-width="374"
-  >
-    <v-img
-      height="250"
-      :src="product.itemImage"
-      cover
-    ></v-img>
+    class="mx-auto"
+    width="800"
+    >
+    <v-card-text class="bg-surface-light pt-4">
+        <v-card-title>Cart
+            {{ total }}
+            <v-btn
+            color="#F0F"
+            text="Checkout"
+        ></v-btn>
+        </v-card-title>
+            <v-row>
+            <v-col cols="12" md="3"  v-for="product in cartItems" :key=product>
+        <v-card
+        class="mx-auto my-12"
+        max-width="374"
+    >
+        <v-img
+        height="250"
+        :src="product.itemImage"
+        cover
+        ></v-img>
 
-    <v-card-item>
-      <v-card-title>{{ product.itemName }}</v-card-title>
-    </v-card-item>
+        <v-card-item>
+        <v-card-title>{{ product.itemName }}</v-card-title>
+        </v-card-item>
 
-    <v-card-text>
-     <div class="my-4 text-subtitle-1">
-        {{ product.itemPrice }}
-      </div>
+        <v-card-text>
+        <div class="my-4 text-subtitle-1">
+            {{ product.itemPrice }}
+        </div>
+        </v-card-text>
 
-      <div>{{ product.description }}</div>
-    </v-card-text>
+        <v-divider class="mx-4 mb-1"></v-divider>
 
-    <v-divider class="mx-4 mb-1"></v-divider>
+        <v-card-actions>
+        <v-btn
+            color="#F0F"
+            text="Remove"
+            block
+            border
+            @click="removeFromCart(product)"
+        ></v-btn>
+        </v-card-actions>
+    </v-card>
+    </v-col>
+    </v-row>
+        </v-card-text>
+    </v-card>
 
-    <v-card-actions>
-      <v-btn
-        color="deep-purple-lighten-2"
-        text="Add to Cart"
-        block
-        border
-      ></v-btn>
-    </v-card-actions>
-  </v-card>
-</v-col>
-</v-row>
+        <div>
+        <v-row>
+            <v-col cols="12" md="3"  v-for="product in stockDetails" :key=product>
+        <v-card
+        class="mx-auto my-12"
+        max-width="374"
+    >
+        <v-img
+        height="250"
+        :src="product.itemImage"
+        cover
+        ></v-img>
+
+        <v-card-item>
+        <v-card-title>{{ product.itemName }}</v-card-title>
+        </v-card-item>
+
+        <v-card-text>
+        <div class="my-4 text-subtitle-1">
+            {{ product.itemPrice }}
+        </div>
+
+        <div>{{ product.description }}</div>
+        </v-card-text>
+
+        <v-divider class="mx-4 mb-1"></v-divider>
+
+        <v-card-actions>
+        <v-btn
+            color="#F0F"
+            text="Add to Cart"
+            block
+            border
+            @click="addToCart(product)"
+        ></v-btn>
+        </v-card-actions>
+    </v-card>
+    </v-col>
+    </v-row>
     </div>
 </template>
 <script>
@@ -46,44 +99,6 @@ export default{
         return{
             products:[1,2,3,4,5,6,7,8,9,10],
             stockDetails : [
-            {
-                itemNumber: 1,
-                itemName : "Gummy bears",
-                itemPrice : 5,
-                itemQuantity : 30,
-                itemImage: 'public/images/Gummy bear.jpeg',
-                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
-            },
-            {
-                itemNumber: 27466,
-                itemName : "Tropical sweets",
-                itemPrice : 2,
-                itemQuantity : 4,
-                itemImage: 'public/images/Tropical sweets.jpeg',
-                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
-
-            }
-            ,
-            {
-                itemNumber: 27466,
-                itemName : "Chocolate",
-                itemPrice : 80,
-                itemQuantity : 4,
-                itemImage: 'public/images/Cadbury chocolate.jpeg',
-                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
-
-            }
-            ,
-            {
-                itemNumber: 27466,
-                itemName : "Skittles",
-                itemPrice : 80,
-                itemQuantity : 4,
-                itemImage: 'public/images/Skittles.jpeg',
-                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
-
-            }
-            ,
             {
                 itemNumber: 27466,
                 itemName : "Snickers",
@@ -134,6 +149,44 @@ export default{
             }
             ,
             {
+                itemNumber: 1,
+                itemName : "Gummy bears",
+                itemPrice : 5,
+                itemQuantity : 30,
+                itemImage: 'public/images/Gummy bear.jpeg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+            },
+            {
+                itemNumber: 27466,
+                itemName : "Tropical sweets",
+                itemPrice : 2,
+                itemQuantity : 4,
+                itemImage: 'public/images/Tropical sweets.jpeg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
+                itemNumber: 27466,
+                itemName : "Chocolate",
+                itemPrice : 80,
+                itemQuantity : 4,
+                itemImage: 'public/images/Cadbury chocolate.jpeg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
+                itemNumber: 27466,
+                itemName : "Skittles",
+                itemPrice : 80,
+                itemQuantity : 4,
+                itemImage: 'public/images/Skittles.jpeg',
+                description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
+
+            }
+            ,
+            {
                 itemNumber: 27466,
                 itemName : "Lollipop",
                 itemPrice : 10,
@@ -143,10 +196,26 @@ export default{
 
             }
         ],
+        total:0,
+
+        cartItems:[],
         }
     },
 
-    methods(){
+    methods:{
+        calculateTotal(itemPrice){
+            this.total+= itemPrice
+
+        },
+        addToCart(product){
+            this.cartItems.push(product)
+            this.calculateTotal(product.itemPrice)
+
+        },
+        removeFromCart(product){
+            this.cartItems.pop(product)
+            this.total -= product.itemPrice
+        }
 
     }
 }
